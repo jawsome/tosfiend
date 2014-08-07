14c632d941515caac6e66f26d33b2c24c3589546
Andrew Tynefield <atynefield@gmail.com>
2014-08-07 03:37:13 -0500

    Initial commit

---
+++ hostgator.com/hostgator.com.tf
@@ -0,0 +1,10 @@
+{
+  "name": "hostgator.com",
+  "sources": {
+    "tos": "http://www.hostgator.com/tos",
+    "tos-vps": "http://www.hostgator.com/tos/vps-tos",
+    "tos-acceptable-use": "http://www.hostgator.com/tos/acceptable-use-policy",
+    "tos-mail-policy": "http://www.hostgator.com/mailpolicy",
+    "tos-dedicated-mail-policy": "http://www.hostgator.com/dedicated-mailpolicy"
+  }
+}
